785338f8975cdf01e2bdd8c76c1c5b1f220d5e30
Marcos Vinicius Volpato <mv.volpato@hotmail.com>
2015-10-02 19:32:01 -0300

    Update pilhaNum.c

--- pilhaNum.c
+++ pilhaNum.c
@@ -2,9 +2,9 @@
  * date: 20/09/2015
  * description: Exercicio de ALG1.
  *				Consiste em uma pilha de inteiros
- *				Deve-se criar uma pilha e usar a função iniciar().
- *				Para empilhar deve-se usar o a função insertNum().
- *				E para desempilhar deve-se usar a função getNum().
+ *				Deve-se criar uma pilha e usar a funÃ§Ã£o iniciar().
+ *				Para empilhar deve-se usar o a funÃ§Ã£o insertNum().
+ *				E para desempilhar deve-se usar a funÃ§Ã£o getNum().
  * 
  * Authors:	 	name: Allan Ribeiro da Costa
  *				uri: https://github.com/allan074
@@ -20,7 +20,7 @@
 #include "pilhaNum.h"
 #define RUN_CODES 0
 
-//aloca espaço para uma pilha e retorna seu ponteiro
+//aloca espaÃ§o para uma pilha e retorna seu ponteiro
 void *inicia(){
 	pilha *p = (pilha *)malloc(sizeof(pilha));
 	if(p != NULL){
@@ -35,7 +35,7 @@ int isEmpty(pilha *p){
 	return (p->topo == NULL);
 }
 
-//aloca espaço para um nó e insere no nó o int recebido como parametro
+//aloca espaÃ§o para um nÃ³ e insere no nÃ³ o int recebido como parametro
 no *aloca(int num){
 	no *novo=(no *) malloc(sizeof(no));
 	if(!novo){
@@ -48,7 +48,7 @@ no *aloca(int num){
 	}
 }
 
-//deleta completamente uma pilha liberando o espaço antes alocado em memória
+//deleta completamente uma pilha liberando o espaÃ§o antes alocado em memÃ³ria
 void deletaPilha(pilha **p){
 	if(!isEmpty(*p)){
 		no *aux = (*p)->topo;
@@ -63,7 +63,7 @@ void deletaPilha(pilha **p){
 	*p = NULL;
 }
 
-//insere um nó no topo da pilha
+//insere um nÃ³ no topo da pilha
 void push(pilha *p, int num){
 	if(isEmpty(p)){
 		p->topo = aloca(num);
@@ -77,11 +77,11 @@ void push(pilha *p, int num){
 	p->tamanho++;
 }
 
-//retira o no do topo e retorna o int desse nó
+//retira o no do topo e retorna o int desse nÃ³
 int pop(pilha *p){
 	if(isEmpty(p)){
 		if(!RUN_CODES){
-			printf("Erro fatal de pilha! O programa será encerrado.");
+			printf("Erro fatal de pilha! O programa serÃ¡ encerrado.");
 			system("pause");
 		}
 		exit(0);
@@ -102,20 +102,22 @@ int pop(pilha *p){
 	}
 }
 
-//imprime na tela todos os int dos nós empilhados atualmente
+//imprime na tela todos os int dos nÃ³s empilhados atualmente
 void consulta(pilha *p){
 	//printf("\n\nResultado da pesquisa:\n");
 	no *aux = p->topo;
 	while(aux != NULL){
-		printf("%d ", aux->num);
+		printf("%d", aux->num);
 		if(aux->anterior != NULL)
 			aux = aux->anterior;
-		else
+		else{
+			printf(" ");
 			break;
+		}
 	}
 }
 
-//imprime na tela todos os int dos nós empilhados atualmente, mas começando da base, nao do topo
+//imprime na tela todos os int dos nÃ³s empilhados atualmente, mas comeÃ§ando da base, nao do topo
 void consulta_inversa(pilha *p){
 	pilha *p2 = inicia();
 	no *aux = p->topo;
@@ -145,7 +147,7 @@ int tamanho(pilha *p){
 	return p->tamanho;
 }
 
-//retorna o nó que está no topo da pilha
+//retorna o nÃ³ que estÃ¡ no topo da pilha
 int *topo(pilha *p){
 	if(!isEmpty(p))
 		return &(p->topo->num);
